f551eb845c9a049e57e95c93586f58c4c9d585cb
coinator <mattvass95@gmail.com>
2021-11-22 01:13:37 +0100

    Correct dap settings, and fix its issues with venv

--- lua/debug-settings.lua
+++ lua/debug-settings.lua
@@ -12,16 +12,18 @@ dap.configurations.python = {
 
     -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
-    program = "${file}"; -- This configuration will launch the current file if used.
+    program = "${workspaceFolder}/${file}"; -- This configuration will launch the current file if used.
+    console='integratedTerminal';
     pythonPath = function()
       -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
       -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
       -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-      local cwd = vim.fn.getcwd()
-      if vim.fn.executable(cwd .. '/venv/bin/python') then
-        return cwd .. '/venv/bin/python'
+      
+      local venv = vim.fn.getenv('VIRTUAL_ENV')
+      if venv ~= vim.NIL then
+	return venv .. '/bin/python'
       else
-        return '/usr/bin/python'
+        return '/usr/bin/python3'
       end
     end;
   },
--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -25,8 +25,3 @@ cmp.setup({
 		{ name = 'latex_symbols' },
 	}
 })
-
--- Setup lspconfig.
-require('lspconfig')["pyright"].setup {
-	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-}

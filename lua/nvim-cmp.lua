d94b6f6be1d98d83001f41726460c80f22b26f52
coinator <mattvass95@gmail.com>
2022-01-03 17:41:53 +0100

    Revert "Add treesitter objects"
    This reverts commit 26bc41be464020474518c8540e2c7d7849ca9e95.

--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -1,6 +1,34 @@
-
 -- Setup nvim-cmp.
 local cmp = require'cmp'
+
+local source = require'cmp_latex_symbols'
+
+local ts_utils = require("nvim-treesitter.ts_utils")
+local items = require("cmp_latex_symbols/items")
+source.is_available = function()
+  if vim.bo.filetype == 'tex' then
+    return true
+  elseif vim.bo.filetype == 'python' then
+    --local node = ts_utils.get_node_at_cursor()
+    return true
+  end
+  return false
+end
+
+source.complete = function(self, request, callback)
+  if not vim.regex(self.get_keyword_pattern() .. "$"):match_str(request.context.cursor_before_line) then
+    return callback()
+  end
+  if not self.items then
+    self.items = items
+  end
+  local node = ts_utils.get_node_at_cursor()
+  if (node:type() == 'string') then 
+    callback(self.items)
+  end
+end
+---Register custom source to nvim-cmp.
+require('cmp').register_source('custom_latex_symbols', source)
 
 cmp.setup({
 	snippet = {
@@ -19,8 +47,7 @@ cmp.setup({
 
 		{ name = 'ultisnips' },
 
-		{ name = 'latex_symbols' },
-
 		{ name = 'nvim_lua' },
+		{ name = 'custom_latex_symbols' },
 	}
 })

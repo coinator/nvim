b426004e7c83c208b45bccb8545b4cadfd45a8f8
coinator <mattvass95@gmail.com>
2022-01-04 22:06:36 +0100

    Add cmp latex for python strings only

--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -1,6 +1,32 @@
-
 -- Setup nvim-cmp.
 local cmp = require'cmp'
+
+local source = require'cmp_latex_symbols'
+local ts_utils = require'nvim-treesitter.ts_utils'
+local items = require("cmp_latex_symbols/items")
+
+function source:is_available()
+  return (vim.bo.filetype == 'tex' or vim.bo.filetype == 'python')
+end
+
+function source:complete(request, callback)
+  local node = ts_utils.get_node_at_cursor()
+  if not vim.regex(self.get_keyword_pattern() .. "$"):match_str(request.context.cursor_before_line) then
+    return callback()
+  end
+  if not self.items then
+    self.items = items
+  end
+  if vim.bo.filetype == 'python' then
+    if node:type() == 'string' then
+      callback(self.items)
+    end
+  else
+      callback(self.items)
+  end
+end
+
+require('cmp').register_source('custom_latex_symbols', source.new())
 
 cmp.setup({
 	snippet = {
@@ -19,7 +45,7 @@ cmp.setup({
 
 		{ name = 'ultisnips' },
 
-		{ name = 'latex_symbols' },
+		{ name = 'custom_latex_symbols' },
 
 		{ name = 'nvim_lua' },
 	}

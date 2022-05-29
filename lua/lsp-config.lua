f6db57da7ced278c041aade13e9ab89cd8db600f
coinator <mattvass95@gmail.com>
2022-05-29 23:13:55 +0200

    Reinstate pyright, pylsp is too slow for numpy etc

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -37,7 +37,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pylsp', "vimls" }
+local servers = { 'pyright', "vimls" }
 --local servers = { 'pyright', "vimls" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {

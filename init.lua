f8d3ae7939a72a7eca713101e158504ff48f1eb3
coinator <mattvass95@gmail.com>
2023-01-04 12:56:17 +0100

    Migrate settings.vim

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/vim-plug/plugins.vim"))
-vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/settings.vim"))
+require("settings")
 vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/augroups.vim"))
 vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/keys.vim"))
 vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/vimtex-settings.vim"))

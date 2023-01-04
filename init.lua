388c4a7f62e023f491b4737ccdb276c457eba1c1
coinator <mattvass95@gmail.com>
2023-01-04 12:29:12 +0100

    Migrate theme to lua

--- init.lua
+++ init.lua
@@ -3,10 +3,10 @@ vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/settings.vim"))
 vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/augroups.vim"))
 vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/keys.vim"))
 vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/vimtex-settings.vim"))
-vim.cmd("source" .. vim.fn.expand("$HOME/.config/nvim/general/theme.vim"))
+require("theme")
 -- source $HOME/.config/nvim/general/lazy-mode.vim
 
-require("lua-snip/init")
+-- require("lua-snip/init")
 --  debug-ui
 
 require("mason").setup()
---
+++ lua/theme.lua
@@ -0,0 +1,22 @@
+vim.opt.termguicolors = true
+vim.cmd "colorscheme onedark"
+
+-- the vertical bar is used to highlight the space
+vim.opt.fillchars = vim.opt.fillchars + "vert: "
+-- vim.opt.showtabline = false
+
+
+
+vim.g.is_transparent = 0
+
+function Toggle_transparent()
+  if vim.g.is_transparent == 0 then
+    vim.cmd "hi Normal guibg=NONE"
+    vim.g.is_transparent = 1
+  else
+    vim.opt.background = dark
+    vim.g.is_transparent = 0
+  end
+end
+
+Toggle_transparent()
